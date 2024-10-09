7f8543868e93e390a54fe193907a35e569dc647d
aharoon2005 <aydin.haroon2005@gmail.com>
2024-10-08 17:59:45 -0700

    initial commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+import numpy as np
+msg="aydin is a boy"
+print(msg.capitalize())       #print "aydin is a boy"
+print(msg)
+msg1=msg.capitalize()
+print(msg1)
+print(np.random.randint(1,9))
+num1=1
+num2=2
+print(num1+num2) 
