945ec4911738d37e9279edb3c78b1ef833f58088
aharoon2005 <aydin.haroon2005@gmail.com>
2024-10-30 08:52:06 -0700

    10/30/24 (1)

---
+++ Week 3/if_else_statements.py
@@ -0,0 +1,7 @@
+if 25>10:
+    print('Correct!')   # if statments controls the flow of a program by executing different code blocks based on whether a condition is true or false
+
+if 25<10:
+    print('Correct!')
+else:
+    print('Wrong!')     #specifies that alternate processing is to take place when the conditions of the matching IF statement are not satisfied
