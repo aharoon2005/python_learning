c944f7ddd8aa974ee07f17da5f8e1275ca916ea2
aharoon2005 <aydin.haroon2005@gmail.com>
2025-01-30 09:33:02 -0800

    1/30

--- CS-1260/ClassLab1.py
+++ CS-1260/ClassLab1.py
@@ -1,15 +1,24 @@
 # Problem 1
+
+# Ask the user to enter two numbers. Print the sum of the two numbers.
 num_1 = input("Enter a number: ")
 num_2 = input("Enter another number: ")
 
+# Print the sum of the two numbers.
 print(f"The sum of {num_1} and {num_2} is {int(num_1) + int(num_2)}")
 
 # problem 2
+
+# Ask the user to enter a number.
 num_1 = int(input("Enter a number: "))
 
+# Print the square of the number
 print(f"The square of {num_1} is {num_1**2}")
 
 # problem 3
+
+# Ask the user to enter a temperature in Celsius.
 temperature = float(input("Enter the temperature in celsius: "))
 
+# Print the temperature in fahrenheit
 print(f"The temperature in fahrenheit is {float(temperature) * 9/5 + 32}")
